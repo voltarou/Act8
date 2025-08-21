c9497898ca8199ae2767c18bbfe76852a9ed1aee
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-21 15:26:35 +0700

    container 4

--- app/src/main/java/com/example/act8/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/act8/repositori/ContainerApp.kt
@@ -7,3 +7,8 @@ interface ContainerApp {
     val repositoriSiswa: RepositoriSiswa
 }
 
+class ContainerDataApp(private val context: Context) : ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
