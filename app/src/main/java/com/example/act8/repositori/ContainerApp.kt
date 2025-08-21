05c12cafeb96853a1664ae19092102bb5893dbd9
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-21 15:26:28 +0700

    container 3

--- app/src/main/java/com/example/act8/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/act8/repositori/ContainerApp.kt
@@ -3,3 +3,7 @@ package com.example.act8.repositori
 import android.content.Context
 import com.example.act8.room.DatabaseSiswa
 
+interface ContainerApp {
+    val repositoriSiswa: RepositoriSiswa
+}
+
