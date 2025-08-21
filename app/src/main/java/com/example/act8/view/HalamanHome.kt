436fbefe5a2af7d7e1b007049834ee4e493fdda0
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-21 16:26:34 +0700

    halaman penyedia

--- app/src/main/java/com/example/act8/view/HalamanHome.kt
+++ app/src/main/java/com/example/act8/view/HalamanHome.kt
@@ -1,2 +1,24 @@
 package com.example.act8.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.act8.R
+import com.example.act8.navigasi.DestinasiNavigasi
+import com.example.act8.viewmodel.HomeViewModel
+import com.example.act8.viewmodel.PenyediaViewModel
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+}
--- app/src/main/java/com/example/act8/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/act8/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.act8.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.act8.AplikasiSiswa
