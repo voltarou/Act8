531c9abd0b744e2e008f14dba1bb026921033d94
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-21 15:57:40 +0700

    entry view 2

--- app/src/main/java/com/example/act8/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/act8/viewmodel/EntryViewModel.kt
@@ -1,2 +1,26 @@
 package com.example.act8.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.act8.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUIState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+}
