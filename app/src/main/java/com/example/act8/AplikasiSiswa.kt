13b2f2cd4e871d4f86f5e1c619e7a1b1fa22f8e6
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-21 15:35:30 +0700

    aplikasi 5

--- app/src/main/java/com/example/act8/AplikasiSiswa.kt
+++ app/src/main/java/com/example/act8/AplikasiSiswa.kt
@@ -4,3 +4,11 @@ import android.app.Application
 import com.example.act8.repositori.ContainerApp
 import com.example.act8.repositori.ContainerDataApp
 
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
