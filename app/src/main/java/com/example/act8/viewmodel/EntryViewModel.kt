899a9e13e5c128f0153f55890a355cfce07447cb
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-21 16:02:59 +0700

    entry view 5

--- app/src/main/java/com/example/act8/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/act8/viewmodel/EntryViewModel.kt
@@ -54,3 +54,14 @@ fun DetailSiswa.toSiswa(): Siswa = Siswa(
     telpon = telpon
 )
 
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
