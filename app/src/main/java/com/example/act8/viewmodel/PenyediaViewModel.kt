39533d4176b7adbd32e90b79b47d728767954690
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-21 16:14:23 +0700

    penyedia view 2

--- app/src/main/java/com/example/act8/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/act8/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.act8.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.act8.AplikasiSiswa
+import com.example.act8.repositori.RepositoriSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
