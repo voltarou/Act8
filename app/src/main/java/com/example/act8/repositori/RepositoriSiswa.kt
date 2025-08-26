0b987de287476f496f4b7a038536b47d4e7cdf3c
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-26 15:23:33 +0700

    edit repos

--- app/src/main/java/com/example/act8/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/act8/repositori/RepositoriSiswa.kt
@@ -8,10 +8,16 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
 
     suspend fun insertSiswa(siswa: Siswa)
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun updateSiswa(siswa: Siswa)
+    suspend fun deleteSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(private val siswaDao: SiswaDao) : RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
 }
