a7d847fc4f201e8514cff4e24dc778637fa89e40
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-26 15:50:41 +0700

    PenyediaViewModel

--- app/src/main/java/com/example/act8/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/act8/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.act8.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -14,6 +15,18 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+            }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
         }
     }
 }
