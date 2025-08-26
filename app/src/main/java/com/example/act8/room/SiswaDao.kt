cf2d3c00c1723b5e5afd7771e22ecdb225aa9a9d
voltarou <161274983+voltarou@users.noreply.github.com>
2025-08-26 15:18:24 +0700

    edit dao

--- app/src/main/java/com/example/act8/room/SiswaDao.kt
+++ app/src/main/java/com/example/act8/room/SiswaDao.kt
@@ -1,9 +1,11 @@
 package com.example.act8.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -11,6 +13,12 @@ interface SiswaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
 
-    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
-    fun getAllSiswa(): Flow<List<Siswa>>
+    @Query("SELECT * FROM tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+    @Update
+    suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
